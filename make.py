4f2b46d8755f32e781aa439d1e3ab95c5acc9fbf
lucasart <lucas.braesch@gmail.com>
2020-12-20 08:44:23 +0800

    don't use test/engine argv[1]
    Files changed: stdout, out2.pgn, log, training.csv.
    
    File signatures:
    % sha1sum stdout out1.pgn out2.pgn log training.csv
    ba27bb7c68f5909d336de0236a341747ade3cc2c  stdout
    9c578a0aa60f9d2e109b914ea43c16bc905a1806  out1.pgn
    e82994696f34d58adb3bcd4fb8f780d0af85f4cf  out2.pgn
    244e46768c6a635fc9ce7f242c87aabfa80a5f01  log
    bddb2ea86343677a1a5307051ca856313e872d68  training.csv
    
    Overall signature:
    % cat stdout out1.pgn out2.pgn log training.csv |sha1sum
    efe78fdb03c17916f05b5bcf9bbc07e50e6332b5  -

--- make.py
+++ make.py
@@ -40,7 +40,7 @@ if args.task == 'test':
             '-openings file=test/chess960.epd order=random srand=1 -repeat -resign 5 900000000 ' \
             '-draw 3 600000000 -games 1930 -pgn out1.pgn 2 -concurrency 8 > /dev/null')
 
-        run('./c-chess-cli -each "cmd=./test/engine 123" depth=3 ' \
+        run('./c-chess-cli -each cmd=./test/engine depth=3 ' \
             '-engine option.Hash=2 tc=10/0 -engine name=e2 tc=20/0 -engine name=e3 ' \
             '-sample 0.5,y,training.csv -openings file=test/chess960.epd -rounds 3 ' \
             '-games 50 -pgn out2.pgn 2 -log > stdout')
